8c712de5859f700e8bafeadf40129c6e697e6007
Kevin <nguyenk215@yahoo.com>
2018-06-13 09:36:28 -0700

    finishes day21

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+function DisplayGifs(props) {
+  return (
+    <div className='container'>
+      {props.list.map((gif) => {
+        return <img className='gif' key={gif.id} src={gif.images.downsized.url}></img>
+      })}
+    </div>
+  )
+}
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      gifData: [],
+    }
+
+    this.handleGenerateGifs = this.handleGenerateGifs.bind(this);
+    this.handleClearGifs = this.handleClearGifs.bind(this);
+  }
+
+  handleGenerateGifs() {
+    const URL = 'https://api.giphy.com/';
+    const ENDPOINT = 'v1/gifs/trending?';
+    const KEY = `api_key=${process.env.REACT_APP_API_KEY}`;
+    const PARAMS = '&limit=25&rating=PG-13';
+    fetch(URL + ENDPOINT + KEY + PARAMS)
+      .then(response => response.json())
+      .then(data => {
+        let gifList = data.data;
+        console.log(gifList);
+        this.setState({
+          gifData: [...gifList],
+        })
+      })
+  }
+
+  handleClearGifs() {
+    this.setState({
+      gifData: [],
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Gifs using the <a href='https://giphy.com/'>GIPHY</a> API</h1>
+        <button onClick={this.handleGenerateGifs}>Load/Reload GIFS</button>
+        <button onClick={this.handleClearGifs}>Clear All Gifs</button>
+        <DisplayGifs list={this.state.gifData} />
+      </div>
+    );
+  }
+}
+
+export default App;
